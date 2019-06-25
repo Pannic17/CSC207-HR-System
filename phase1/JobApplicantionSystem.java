9ad782a5568cec47d68be9667cdcdd3973b06d28
Tejbeer Bhullar <tejbeerbhullar@yahoo.in>
2019-06-25 10:35:34 -0400

    The primary file of the Project that will initiate the program

---
+++ phase1/JobApplicantionSystem.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class JobApplicantionSystem {
+
+    public static ArrayList<JobPosting> Jobs = new ArrayList<>();
+    public static ArrayList<Applicant> Applicants = new ArrayList<>();
+    public static ArrayList<Interviewer> Interviewers = new ArrayList<>();
+
+}
