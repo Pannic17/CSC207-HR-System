2bd64d9deb6ccc80b209af671b21392195a3008b
leilasalem <leila.salem@mail.utoronto.ca>
2019-06-25 18:29:49 -0400

    Testing newly cloned repo

--- phase1/JobApplicantionSystem.java
+++ phase1/JobApplicantionSystem.java
@@ -5,5 +5,6 @@ public class JobApplicantionSystem {
     public static ArrayList<JobPosting> Jobs = new ArrayList<>();
     public static ArrayList<Applicant> Applicants = new ArrayList<>();
     public static ArrayList<Interviewer> Interviewers = new ArrayList<>();
-
+// testing push leila
+    
 }
