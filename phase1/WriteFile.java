973d7dccfe32bbd81dc9e63cca566daf81eefe05
Tejbeer Bhullar <tejbeer.bhullar@mail.utoronto.ca>
2019-07-10 19:11:29 -0400

    reformat code

--- phase1/WriteFile.java
+++ phase1/WriteFile.java
@@ -5,21 +5,20 @@ import java.io.PrintWriter;
 import java.io.IOException;
 
 
-
 public class WriteFile {
     private String path;
     private boolean appendToFile = false;
 
-    public WriteFile(String filePath){
+    public WriteFile(String filePath) {
         path = filePath;
     }
 
-    public WriteFile(String filePath, boolean appendValue){
+    public WriteFile(String filePath, boolean appendValue) {
         path = filePath;
         appendToFile = appendValue;
     }
 
-    public void writeToFile(String textLine) throws IOException{
+    public void writeToFile(String textLine) throws IOException {
         //TODO:  check if the file exists
 
 
@@ -28,12 +27,12 @@ public class WriteFile {
 
         // since file writer writes bytes so we pass plain text using print writer
         PrintWriter printLine = new PrintWriter(write);
-        printLine.printf("%s"+"%n", textLine);   // %s means a string of characters, %n means a new line
+        printLine.printf("%s" + "%n", textLine);   // %s means a string of characters, %n means a new line
         printLine.close();
     }
 
 
-    public void bufferedWriteToFile(String textLine) throws IOException{
+    public void bufferedWriteToFile(String textLine) throws IOException {
         //TODO:  check if the file exists
 
         // file writer takes care of opening the correct file and storing the text as bytes
@@ -41,14 +40,14 @@ public class WriteFile {
 
         // since file writer writes bytes so we pass plain text using print writer
         PrintWriter printLine = new PrintWriter(write);
-        printLine.printf("%s"+"%n", textLine);   // %s means a string of characters, %n means a new line
+        printLine.printf("%s" + "%n", textLine);   // %s means a string of characters, %n means a new line
         printLine.close();
     }
 
 
-    public void createFile(String filename){
+    public void createFile(String filename) {
         // check if the file already exists
-        File file = new File(filename+".txt");
+        File file = new File(filename + ".txt");
     }
 
 
