e82c3da80202e8d393dc4ffbf33c8be4d280a343
Tejbeer Bhullar <tejbeerbhullar@yahoo.in>
2019-07-10 16:31:58 -0400

    New Class : LoginClass

---
+++ phase1/LoginClass.java
@@ -0,0 +1,22 @@
+public class LoginClass {
+
+    public void forgotPassword(){}
+
+    public void newAccount(){}
+
+    public void verifyPassword(){}
+
+    public void applicantLogin(){}
+
+    public void hrLogin(){}
+
+    public void interviewerLogin(){}
+
+    public void createNewApplicant(){}
+
+    public void createNewHR(){}
+
+    public void createNewInterviewer(){}
+
+
+}
