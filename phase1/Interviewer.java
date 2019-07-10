452aed70a2f67331b70386353d0647aad79ffe89
Tejbeer Bhullar <tejbeerbhullar@yahoo.in>
2019-07-10 16:24:43 -0400

    New File Interviewer

---
+++ phase1/Interviewer.java
@@ -0,0 +1,19 @@
+public class Interviewer {
+
+    public void getApplicants(){}
+
+    public void searchApplicants(){}
+
+    public void hireApplicants(){}
+
+    public void createInterviewer(){}
+
+    public void checkStatuses(){}
+
+    public void recommendApplicant(){}
+
+    public void jobDoneNotification(){}
+
+
+
+}
