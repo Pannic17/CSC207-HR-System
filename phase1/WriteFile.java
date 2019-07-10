979b05f58d4a78863daedb5a5089fb0549af9ad4
Tejbeer Bhullar <tejbeerbhullar@yahoo.in>
2019-07-10 18:07:37 -0400

    new methods in Write class

--- phase1/WriteFile.java
+++ phase1/WriteFile.java
@@ -1,7 +1,40 @@
+
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+
+
 public class WriteFile {
+    private String path;
+    private boolean appendToFile = false;
 
-    public void writeFile(){}
+    public WriteFile(String filePath){
+        path = filePath;
+    }
 
-    // TODO: public boolean checkIfFileExists(){}
+    public WriteFile(String filePath, boolean appendValue){
+        path = filePath;
+        appendToFile = appendValue;
+    }
 
+    public void writeToFile(String textLine) throws IOException{
+        //TODO:  check if the file exists
+
+
+        // file writer takes care of opening the correct file and storing the text as bytes
+        FileWriter write = new FileWriter(path, appendToFile);
+
+        // since file writer writes bytes so we pass plain text using print writer
+        PrintWriter printLine = new PrintWriter(write);
+        printLine.printf("%s"+"%n", textLine);   // %s means a string of characters, %n means a new line
+        printLine.close();
+    }
+
+    public void createFile(){}
+
+
+    // TODO: method  -   public boolean checkIfFileExists(){}
+
+  
 }
